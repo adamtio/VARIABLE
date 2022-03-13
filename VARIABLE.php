4379208b23065c9be5f820c9ab8ba72fdd210b0d
AdamTio-18094 <85661116+adamtio@users.noreply.github.com>
2022-03-13 18:51:01 +0700

    Add files via upload

---
+++ VARIABLE.php
@@ -0,0 +1,13 @@
+<?php 
+$a = 4;
+$b = 19;
+//menjumlahkan variabel a dengan variabel b
+echo $a + $b;
+?>
+
+<?php 
+$a = 4;
+$b = 19;
+//Operator pengurangan variabel a dengan variabel b
+echo $a - $a;
+?>
